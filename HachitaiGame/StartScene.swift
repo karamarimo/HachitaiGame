5beb513b27bca1debeb0d15e6f6dad00c8348015
kei kameda <kei1225kk@gmail.com>
2016-09-24 19:32:05 +0900

    modify start button to switch to gemeScene

--- HachitaiGame/StartScene.swift
+++ HachitaiGame/StartScene.swift
@@ -11,12 +11,28 @@ import GameKit
 
 class StartScene: SKScene {
     override func didMove(to view: SKView) {
+        makeView()
+    }
+    
+    private func makeView() {
         let newGameLabel = SKLabelNode(text: "START")
         let newGameButton = SKButton(color: UIColor.orange, size: CGSize(width: 100, height: 100))
-        newGameButton.setCallback {
-            
-        }
+        
+        newGameButton.position = CGPoint(x: self.frame.width / 2, y: self.frame.height / 2)
         newGameButton.addChild(newGameLabel)
         self.addChild(newGameButton)
+        
+        
+        newGameButton.setCallback {
+            self.moveToGameScene()
+        }
+        
+
+    }
+    private func moveToGameScene() {
+        let gameScene = GameScene()
+        self.view?.presentScene(gameScene, transition: SKTransition.doorsOpenHorizontal(withDuration: 0.5) )
     }
 }
+
+
