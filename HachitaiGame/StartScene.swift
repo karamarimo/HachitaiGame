4f043d8c81bb37c65537da5f1dd90a5f04207150
karamarimo <admin@pc-b094.local>
2016-09-24 16:32:02 +0900

    update scenes

---
+++ HachitaiGame/StartScene.swift
@@ -0,0 +1,13 @@
+//
+//  StartScene.swift
+//  HachitaiGame
+//
+//  Created by admin on 2016/09/24.
+//  Copyright © 2016年 admin. All rights reserved.
+//
+
+import UIKit
+
+class StartScene: SKScene {
+
+}
