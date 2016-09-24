b4c0bcc5a95a5874d4f69fb970f62ed5b11096d5
karamarimo <admin@pc-b094.local>
2016-09-24 17:47:35 +0900

    update start scene

--- HachitaiGame/StartScene.swift
+++ HachitaiGame/StartScene.swift
@@ -7,7 +7,16 @@
 //
 
 import UIKit
+import GameKit
 
 class StartScene: SKScene {
-
+    override func didMove(to view: SKView) {
+        let newGameLabel = SKLabelNode(text: "START")
+        let newGameButton = SKButton(color: UIColor.orange, size: CGSize(width: 100, height: 100))
+        newGameButton.setCallback {
+            
+        }
+        newGameButton.addChild(newGameLabel)
+        self.addChild(newGameButton)
+    }
 }
